06cafa78ea027485f53c423e048c1422df041983
George Spake <gpspake@gmail.com>
2016-11-08 22:12:39 -0600

    add feed model and migration

---
+++ database/migrations/2016_11_08_221254_create_feeds_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feeds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('service')->index();
+            $table->integer('post_id');
+            $table->string('image_url');
+            $table->string('content');
+            $table->string('post_url');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feeds');
+    }
+}
