ec4ebc78e7d5d7f950073b6a10f1c173c2799b91
George Spake <gpspake@gmail.com>
2016-11-14 14:10:34 -0600

    add description to get facebook posts command

--- app/Console/Commands/GetFacebookPosts.php
+++ app/Console/Commands/GetFacebookPosts.php
@@ -19,7 +19,7 @@ class GetFacebookPosts extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Get Facebook posts';
 
     /**
      * Create a new command instance.
