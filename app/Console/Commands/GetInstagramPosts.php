a3a8596b76acbe9da90b6f5de7c7936841b57f78
George Spake <gpspake@gmail.com>
2016-11-15 10:42:24 -0600

    move instagram concerns to trait

--- app/Console/Commands/GetInstagramPosts.php
+++ app/Console/Commands/GetInstagramPosts.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 class GetInstagramPosts extends Command
 {
 
-    use SocialMedia;
+    use SocialMedia\Instagram;
 
     /**
      * The name and signature of the console command.
@@ -31,61 +31,20 @@ class GetInstagramPosts extends Command
     protected $service_name = 'instagram';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Get Instagram posts as array
-     *
-     * @return mixed
-     */
-    function getPosts()
-    {
-        $url = 'https://api.instagram.com/v1/users/' . env('instagram_user_id') . '/media/recent';
-        $token = 'access_token=' . env('instagram_user_id') . '.' . env('instagram_access_token');
-        $count = 'count=20';
-
-        $query_string = '?' . $token . '&' . $count;
-
-        $posts = json_decode( file_get_contents( $url . $query_string ), true );
-
-        return $posts['data'];
-    }
-
-    /**
-     * Format single Instagram post
-     *
-     * @param $ig_post
-     */
-    function parsePost($ig_post){
-        $created_at = date('Y-m-d H:i:s', $ig_post['created_time']);
-        $content = array_key_exists('caption', $ig_post) ? substr( $ig_post['caption']['text'] , 0 , 999 ) : '';
-
-
-        $parsed_ig_post = [];
-        $parsed_ig_post['service'] = 'instagram';
-        $parsed_ig_post['post_id'] = $ig_post['id'];
-        $parsed_ig_post['image_url'] = $ig_post['images']['low_resolution']['url'];
-        $parsed_ig_post['content'] = $content;
-        $parsed_ig_post['post_url'] = $ig_post['link'];
-        $parsed_ig_post['published_at'] = $created_at;
-
-        $this->savePost($parsed_ig_post);
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        $this->savePosts();
+        $this->deleteExistingPosts();
+
+        if ($this->option('delete')) {
+            return null;
+        }
+
+        $posts = $this->parsePosts( $this->getPosts() );
+
+        $this->savePosts($posts);
     }
 }
---
+++ app/Console/Commands/SocialMedia/Instagram.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands\SocialMedia;
+
+trait Instagram
+{
+
+    use Storage;
+
+    /**
+     * The service name
+     *
+     * @var string
+     */
+    protected $service_name = 'instagram';
+
+    /**
+     * Get Instagram posts as array
+     *
+     * @return mixed
+     */
+    function getPosts()
+    {
+        $url = 'https://api.instagram.com/v1/users/' . env('instagram_user_id') . '/media/recent';
+        $token = 'access_token=' . env('instagram_user_id') . '.' . env('instagram_access_token');
+        $count = 'count=20';
+
+        $query_string = '?' . $token . '&' . $count;
+
+        $posts = json_decode( file_get_contents( $url . $query_string ), true );
+
+        return $posts['data'];
+    }
+
+    /**
+     * Format single Instagram post
+     *
+     * @param $ig_post
+     * @return array
+     */
+    function parsePost($ig_post){
+        $created_at = date('Y-m-d H:i:s', $ig_post['created_time']);
+        $content = array_key_exists('caption', $ig_post) ? substr( $ig_post['caption']['text'] , 0 , 999 ) : '';
+        
+        $parsed_ig_post = [];
+        $parsed_ig_post['service'] = 'instagram';
+        $parsed_ig_post['post_id'] = $ig_post['id'];
+        $parsed_ig_post['image_url'] = $ig_post['images']['low_resolution']['url'];
+        $parsed_ig_post['content'] = $content;
+        $parsed_ig_post['post_url'] = $ig_post['link'];
+        $parsed_ig_post['published_at'] = $created_at;
+
+        return $parsed_ig_post;
+    }
+
+
+    /**
+     * Return an array of formatted posts
+     *
+     * @param $posts
+     * @return array
+     */
+    function parsePosts($posts)
+    {
+        return array_map(array($this, 'parsePost'), $posts);
+    }
+}
