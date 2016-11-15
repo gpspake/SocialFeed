46242565c177d0f20c2d1ce543e04586027354e3
George Spake <gpspake@gmail.com>
2016-11-15 10:21:28 -0600

    move facbook concerns to trait

--- app/Console/Commands/GetFacebookPosts.php
+++ app/Console/Commands/GetFacebookPosts.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 class GetFacebookPosts extends Command
 {
 
-    use SocialMedia;
+    use SocialMedia\Facebook;
 
     /**
      * The name and signature of the console command.
@@ -22,72 +22,5 @@ class GetFacebookPosts extends Command
      * @var string
      */
     protected $description = 'Get Facebook posts';
-
-    /**
-     * The service name
-     *
-     * @var string
-     */
-    protected $service_name = 'facebook';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
     
-    /**
-     * Get fb posts as array
-     *
-     * @return mixed
-     */
-    public function getPosts()
-    {
-        $url = 'https://graph.facebook.com/uthsc/posts';
-        $access_token = 'access_token=' . env('FACEBOOK_APP_ID') . '|' . env('FACEBOOK_APP_SECRET');
-        $fields = 'fields=message,created_time,full_picture,permalink_url';
-        $limit = 'limit=80';
-        $query_string = '?' . $access_token . '&' . $fields . '&' . $limit;
-
-        $posts = json_decode( file_get_contents ( $url . $query_string ), true );
-
-        return $posts['data'];
-    }
-
-    /**
-     * Parse single fb posts
-     *
-     * @param $fb_post
-     */
-    function parsePost($fb_post)
-    {
-        $created_at = date('Y-m-d H:i:s', strtotime($fb_post['created_time']));
-        $content = array_key_exists('message', $fb_post) ? substr( $fb_post['message'] , 0 , 999 ) : '';
-        $image = array_key_exists('image_url', $fb_post) ? $fb_post['image_url'] : '';
-
-        $parsed_fb_post = [];
-        $parsed_fb_post['service'] = 'facebook';
-        $parsed_fb_post['post_id'] = $fb_post['id'];
-        $parsed_fb_post['image_url'] = $image;
-        $parsed_fb_post['content'] = $content;
-        $parsed_fb_post['post_url'] = $fb_post['permalink_url'];
-        $parsed_fb_post['published_at'] = $created_at;
-
-        $this->savePost($parsed_fb_post);
-
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->savePosts();
-    }
 }
---
+++ app/Console/Commands/SocialMedia/Facebook.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands\SocialMedia;
+
+trait Facebook
+{
+
+    use Storage;
+
+    /**
+     * The service name
+     *
+     * @var string
+     */
+    protected $service_name = 'facebook';
+
+    /**
+     * Get fb posts as array
+     *
+     * @return mixed
+     */
+    public function getPosts()
+    {
+        $url = 'https://graph.facebook.com/uthsc/posts';
+        $access_token = 'access_token=' . env('FACEBOOK_APP_ID') . '|' . env('FACEBOOK_APP_SECRET');
+        $fields = 'fields=message,created_time,full_picture,permalink_url';
+        $limit = 'limit=80';
+        $query_string = '?' . $access_token . '&' . $fields . '&' . $limit;
+
+        $posts = json_decode( file_get_contents ( $url . $query_string ), true );
+
+        return $posts['data'];
+    }
+
+    /**
+     * Parse single fb post
+     *
+     * @param $fb_post
+     * @return array
+     */
+    function parsePost($fb_post)
+    {
+        $created_at = date('Y-m-d H:i:s', strtotime($fb_post['created_time']));
+        $content = array_key_exists('message', $fb_post) ? substr( $fb_post['message'] , 0 , 999 ) : '';
+        $image = array_key_exists('image_url', $fb_post) ? $fb_post['image_url'] : '';
+
+        $parsed_fb_post = [];
+        $parsed_fb_post['service'] = 'facebook';
+        $parsed_fb_post['post_id'] = $fb_post['id'];
+        $parsed_fb_post['image_url'] = $image;
+        $parsed_fb_post['content'] = $content;
+        $parsed_fb_post['post_url'] = $fb_post['permalink_url'];
+        $parsed_fb_post['published_at'] = $created_at;
+
+        return $parsed_fb_post;
+    }
+
+    /**
+     * Return an array of formatted posts
+     *
+     * @param $posts
+     * @return array
+     */
+    function parsePosts($posts)
+    {
+        return array_map(array($this, 'parsePost'), $posts);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $posts = $this->parsePosts( $this->getPosts() );
+
+        $this->savePosts($posts);
+    }
+}
